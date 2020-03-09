ef34a1193fffe4e3ec97b2c25c789bde5ad3da29
Doyle <zyrek.g@gmail.com>
2020-03-09 11:38:08 +0100

    Commented out not working test for vert.x

--- src/main/java/pl/training/gzyrek/offer/service/OfferService.java
+++ src/main/java/pl/training/gzyrek/offer/service/OfferService.java
@@ -1,5 +1,8 @@
 package pl.training.gzyrek.offer.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.training.gzyrek.offer.OfferVerticle;
 import pl.training.gzyrek.offer.model.Offer;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,26 +14,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OfferService {
-
+    private static Logger logger = LoggerFactory.getLogger(OfferService.class);
 
     public List<Offer> prepareOffers(String input) {
         List<Offer> offers = new ArrayList<>();
+        logger.debug("Parsing input from: " + input);
         Document doc = Jsoup.parse(input);
+        logger.debug("Parsed document: " + doc);
         Element offersTable = doc.getElementById("offers_table");
-        Elements tableBody = offersTable.getElementsByAttribute("data-id");
-        tableBody.forEach(element -> {
-            Elements details = element.getElementsByTag("strong");
-            offers.add(Offer.builder()
-                    .id(Long.valueOf(element.attributes().get("data-id")))
-                    .name(details.get(0).ownText())
-                    .price(new BigDecimal(details.get(1)
-                            .ownText()
-                            .trim()
-                            .replace("zł", "")
-                            .replace(" ", "")
-                            .replace(",", ".")
-                    )).build());
-        });
+        logger.debug("Got offers table: " + offersTable);
+        if(offersTable!=null) {
+            Elements tableBody = offersTable.getElementsByAttribute("data-id");
+            logger.debug("Got table content: " + tableBody);
+            tableBody.forEach(element -> {
+                Elements details = element.getElementsByTag("strong");
+                offers.add(Offer.builder()
+                        .id(Long.valueOf(element.attributes().get("data-id")))
+                        .name(details.get(0).ownText())
+                        .price(new BigDecimal(details.get(1)
+                                .ownText()
+                                .trim()
+                                .replace("zł", "")
+                                .replace(" ", "")
+                                .replace(",", ".")
+                        )).build());
+            });
+        }
+        logger.debug("Produced list of offers: " + offers.toString());
         return offers;
     }
 }
--- src/test/java/pl/training/gzyrek/offer/OfferServiceTest.java
+++ src/test/java/pl/training/gzyrek/offer/OfferServiceTest.java
@@ -1,4 +1,57 @@
 package pl.training.gzyrek.offer;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import pl.training.gzyrek.offer.model.Offer;
+import pl.training.gzyrek.offer.service.OfferService;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class OfferServiceTest {
+    private OfferService offerService = new OfferService();
+    @Test
+    public void shouldPrepareOffersFromInput() throws IOException, URISyntaxException {
+        URL res = getClass().getClassLoader().getResource("test.html");
+        String input = new String (Files.readAllBytes(Paths.get(Objects.requireNonNull(res).toURI())));
+        List<Offer> offers = offerService.prepareOffers(input);
+
+        assertNotNull(offers);
+        assertFalse(offers.isEmpty());
+        assertEquals(4, offers.size());
+
+        assertEquals(586727107, offers.get(0).getId());
+        assertEquals("Felgi 18 cali Honda Hyundai Mitsubishi Kia Nissan 5 x 114.3 Et38",offers.get(0).getName());
+        assertEquals(BigDecimal.valueOf(1100), offers.get(0).getPrice());
+
+        assertEquals(589670661, offers.get(1).getId());
+        assertEquals("HYUNDAI GALLOPER 00 2.5TDI SILNIK D4BH 150TYŚ FV",offers.get(1).getName());
+        assertEquals(BigDecimal.valueOf(2800), offers.get(1).getPrice());
+
+        assertEquals(586767558, offers.get(2).getId());
+        assertEquals("Hyundai ix20 Szklany Dach 1.6 CRDi 116KM PDC Klimatronic",offers.get(2).getName());
+        assertEquals(BigDecimal.valueOf(24900), offers.get(2).getPrice());
+
+        assertEquals(562405190, offers.get(3).getId());
+        assertEquals("Hyundai I30 1.6 CRDI*Lift* ZAMIANA*Po Opłatach*Niemiecki*Bezwypadkowy*SUPER STAN",offers.get(3).getName());
+        assertEquals(BigDecimal.valueOf(16900), offers.get(3).getPrice());
+
+    }
+    @Test
+    public void shouldReturnEmptyListForNonHtmlInput(){
+        String input = "Some random string";
+        List<Offer> offers = offerService.prepareOffers(input);
+        assertNotNull(offers);
+        assertTrue(offers.isEmpty());
+    }
 }
